13efe3c456b0671914155f99a5a8674d2b904c99
NikolaSretko <nikola.sretkovic99@gmail.com>
2024-02-27 16:20:51 +0100

    hot fix

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,7 @@
 import './Button.scss'
 const Button = ({ text }) => {
     return ( 
-        <a download="Nikola_Sretkovic_CV" className="button" href="../../../public/Nikola Sretkovic CV .pdf">CV</a>
+        <a download="Nikola_Sretkovic_CV" className="button" href="/Nikola Sretkovic CV .pdf">CV</a>
     );
 }
 
