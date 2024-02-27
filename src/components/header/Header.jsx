38280a05b8a4c19a735cdd5c4780a43cd3f660ae
NikolaSretko <nikola.sretkovic99@gmail.com>
2024-02-27 16:14:05 +0100

    cv

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import Nav from './Nav';
 import './Header.scss'
 import Button from '../button/Button';
-import GitHub from '../../assets/svg/GitHub';
 import In from '../../assets/svg/In';
 
 const Header = ({ setzeDenLightModus }) => {
@@ -18,7 +17,7 @@ const Header = ({ setzeDenLightModus }) => {
 Hallo, ich bin Nikki, eine erfahrene Foto- und Mediendesignerin, Musikproduzentin und neuerdings Frontend-Webentwicklerin, die sich zum Ziel gesetzt hat, bis zum 22. März eine zertifizierte Fullstack-Entwicklerin im MERN-Stack zu werden. Meine Reise hat mich kürzlich zur großen Liebe für das Backend geführt, gerade als ich den Backend-Teil meines Studiums abschließe. Diese Entdeckung hat meine Begeisterung für technische Herausforderungen und Problemlösungen geweckt. Ich bin aktiv auf der Suche nach spannenden Projekten, die meine vielfältigen Fähigkeiten herausfordern, also zögern Sie nicht, Kontakt aufzunehmen, um mehr zu erfahren.
             </p>
             <Button text="CV"
-            target="_blank" href="/Nikola Sretkovic CV .pdf"
+            target="_blank" href="../../assets/Nikola Sretkovic CV .pdf"
             />
             <a target="_blank" href="https://www.linkedin.com/in/nikola-sretkovic-aa8b322a7/"><In/></a>                
         </header>
