0bd47ee678ef796bd7fb2aeab8adc0fde74a30f6
Nikolas Sretkovic <145345708+NikolaSretko@users.noreply.github.com>
2024-05-20 14:31:08 +0200

    übermich

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,39 @@
-import Nav from './Nav';
-import './Header.scss'
-import Button from '../button/Button';
-import In from '../../assets/svg/In';
+import Nav from "./Nav";
+import "./Header.scss";
+import Button from "../button/Button";
+import In from "../../assets/svg/In";
 
 const Header = ({ setzeDenLightModus }) => {
+  return (
+    <div>
+      <header className="headerContainer">
+        <Nav setzeDenLightModus={setzeDenLightModus} />
+        <h1>
+          Hi, ich bin <span>Nikola Sretkovic.</span>
+        </h1>
+        <h2>Fullstack Developer.</h2>
+        <p>
+          Hallo, ich bin Nikki, ein erfahrener Foto- und Mediendesigner,
+          Musikproduzent und zertifizierter Fullstack-Entwickler im MERN-Stack.
+          Nach erfolgreichem Abschluss meiner Ausbildung und meiner
+          Zertifizierung am 22. März arbeite ich jetzt als
+          Fullstack-Softwareentwickler bei einem Unternehmen. Meine Reise hat
+          mich zur großen Liebe für das Backend geführt und meine Begeisterung
+          für technische Herausforderungen und Problemlösungen weiter
+          gesteigert. Ich bin immer auf der Suche nach neuen, spannenden
+          Projekten, die meine vielfältigen Fähigkeiten herausfordern. Zögern
+          Sie nicht, Kontakt aufzunehmen, um mehr zu erfahren.
+        </p>
 
-    return ( 
-        <div>
-        <header className='headerContainer'>
-            <Nav
-            setzeDenLightModus={setzeDenLightModus}
-            />
-            <h1>Hi, ich bin  <span>Nikola Sretkovic.</span></h1>
-            <h2>Fullstack Developer.</h2>
-            <p>
-Hallo, ich bin Nikki, eine erfahrene Foto- und Mediendesignerin, Musikproduzentin und neuerdings Frontend-Webentwicklerin, die sich zum Ziel gesetzt hat, bis zum 22. März eine zertifizierte Fullstack-Entwicklerin im MERN-Stack zu werden. Meine Reise hat mich kürzlich zur großen Liebe für das Backend geführt, gerade als ich den Backend-Teil meines Studiums abschließe. Diese Entdeckung hat meine Begeisterung für technische Herausforderungen und Problemlösungen geweckt. Ich bin aktiv auf der Suche nach spannenden Projekten, die meine vielfältigen Fähigkeiten herausfordern, also zögern Sie nicht, Kontakt aufzunehmen, um mehr zu erfahren.
-            </p>
-            <a target="_blank" href="https://www.linkedin.com/in/nikola-sretkovic-aa8b322a7/"><In/></a>                
-        </header>
-        </div>
-    );
-}
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/nikola-sretkovic-aa8b322a7/"
+        >
+          <In />
+        </a>
+      </header>
+    </div>
+  );
+};
 
 export default Header;
