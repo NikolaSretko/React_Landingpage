82b4df3db02608335ee0bf8ce54b9b74f63f7655
Nikolas Sretkovic <145345708+NikolaSretko@users.noreply.github.com>
2024-05-22 23:55:20 +0200

    fix skill buttons

--- src/components/skillsection/Skill.jsx
+++ src/components/skillsection/Skill.jsx
@@ -1,22 +1,31 @@
-import './Skill.scss'
-const Skill = ({skill,skill2,skill3,skill4,skill5,skill6,skill7,skill8}) => {
-    return ( 
-        <>
-        <section className='skillsContainer'>
-            <h2>SKILLS</h2>
-            <div className='singelSkillWrapper'>
-                <a href="#">HTML</a>
-                <a href="#">SCSS</a>
-                <a href="#" className='item3'>JavaScript</a>
-                <a href="#">React</a>
-                <a href="#">MERN</a>
-                <a href="#" className='item6'>Tailwind</a>
-                <a href="#"> Git</a>
-                <a href="#">Express</a>
-            </div>
-        </section>  
-        </>
-    );
-}
+import "./Skill.scss";
+const Skill = ({
+  skill,
+  skill2,
+  skill3,
+  skill4,
+  skill5,
+  skill6,
+  skill7,
+  skill8,
+}) => {
+  return (
+    <>
+      <section className="skillsContainer">
+        <h2>SKILLS</h2>
+        <div className="singelSkillWrapper">
+          <p>HTML</p>
+          <p>SCSS</p>
+          <p className="item3">JavaScript</p>
+          <p>React</p>
+          <p>MERN</p>
+          <p className="item6">Tailwind</p>
+          <p> Git</p>
+          <p>Express</p>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Skill;
