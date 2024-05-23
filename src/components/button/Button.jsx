dc42b903363cd2a7cb5ac55d391abb15e65d0eeb
Nikolas Sretkovic <145345708+NikolaSretko@users.noreply.github.com>
2024-05-23 02:35:23 +0200

    CV s/w

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,14 +1,26 @@
 import './Button.scss';
 const Button = () => {
   return (
-    <a
-      download="Nikola_Sretkovic_CV"
-      className="button"
-      href="https://res.cloudinary.com/dxphdtwvh/image/upload/f_auto,q_auto/v1/CV/njvtjkugywiwqbbnqfwx"
-      target="blank"
-    >
-      CV
-    </a>
+    <div className="button-container">
+      <a
+        download="Nikola_Sretkovic_CV"
+        className="button"
+        href="https://res.cloudinary.com/dxphdtwvh/image/upload/f_auto,q_auto/v1/CV/hl6svavdikjn3gxetduh"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        CV
+      </a>
+      <a
+        download="Another_Document"
+        className="button"
+        href="https://res.cloudinary.com/dxphdtwvh/image/upload/f_auto,q_auto/v1/CV/nming9wunydbjjfecwmn"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        s/w
+      </a>
+    </div>
   );
 };
 
