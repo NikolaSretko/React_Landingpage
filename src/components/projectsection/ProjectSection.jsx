8bcb66cef39cf7c01b10840bd5e26e5ae47e91bd
Nikolas Sretkovic <145345708+NikolaSretko@users.noreply.github.com>
2024-05-23 10:24:19 +0200

    typsafe

--- src/components/projectsection/ProjectSection.jsx
+++ src/components/projectsection/ProjectSection.jsx
@@ -1,14 +1,11 @@
-import CSS from "../../assets/svg/CSS";
-import GitHub from "../../assets/svg/GitHub";
-import HTML5 from "../../assets/svg/HTML5";
-import JavaScript from "../../assets/svg/JavaScript";
-import Link from "../../assets/svg/Link";
-import MERN from "../../assets/svg/MERN";
-import React from "../../assets/svg/React";
-import Sass from "../../assets/svg/SCSS";
-import Tailwind from "../../assets/svg/Tailwind";
-import Project from "./Project";
-import "./ProjectSection.scss";
+import GitHub from '../../assets/svg/GitHub';
+import JavaScript from '../../assets/svg/JavaScript';
+import Link from '../../assets/svg/Link';
+import MERN from '../../assets/svg/MERN';
+import React from '../../assets/svg/React';
+import Sass from '../../assets/svg/SCSS';
+import Project from './Project';
+import './ProjectSection.scss';
 
 const ProjectSection = () => {
   return (
@@ -17,12 +14,17 @@ const ProjectSection = () => {
       <div>
         <Project
           projectTitle="TOKTOK(Mobile)"
-          projectDescription="Unsere mobile App, gebaut im MERN-Stack, bietet eine sichere und skalierbare Plattform zum Teilen von Fotos. Ich war hauptsächlich für das robuste Backend zuständig, das Sicherheit und Performance gewährleistet. Zusätzlich habe ich das Frontend und die Fotofunktionen in der Endphase übernommen, um ein nahtloses Benutzererlebnis zu schaffen."
-          technologies={[<Sass />, <React />, <MERN />]}
+          projectDescription="Unsere mobile App, gebaut im MERN-Stack, bietet eine sichere und skalierbare Plattform zum Teilen von Fotos. Ich war hauptsächlich für das robuste Backend zuständig, das Sicherheit und Performance gewährleistet. Zusätzlich habe ich das Frontend und die Fotofunktionen in der Endphase übernommen, um ein nahtloses Benutzererlebnis zu schaffen. Da wir einen kostenlosen NoSQL-Dienstleister verwenden, kann das initiale Laden der mobilen Web-App verzögert sein. In dieser Zeit kann man über den Desktop bereits die mobile Ansicht aktivieren."
+          technologies={[
+            <Sass key="sass" />,
+            <React key="react" />,
+            <MERN key="mern" />,
+          ]}
           repo={
             <a
-              target="blank"
               href="https://github.com/canyus70/FullStack-Abschluss-Projekt-TokTok"
+              target="_blank"
+              rel="noreferrer"
             >
               <GitHub />
             </a>
@@ -36,7 +38,11 @@ const ProjectSection = () => {
         <Project
           projectTitle="Pokedex"
           projectDescription="Ein Teamprojekt, das eine Pokedex-API nutzt. Ich war zuständig für die Hauptseite, einschließlich der Implementierung des Dark Modes und der Erstellung von CSS-Animationen. Diese Animationen sorgen für eine interaktive und ansprechende Nutzererfahrung. Zusätzlich habe ich sicher gestellt, dass die Hauptseite sowohl funktional als auch ästhetisch ansprechend ist, wodurch die Benutzerfreundlichkeit und das visuelle Erlebnis verbessert wurden."
-          technologies={[<React />, <JavaScript />, <Sass />]}
+          technologies={[
+            <React key="react" />,
+            <JavaScript key="javascript" />,
+            <Sass key="sass" />,
+          ]}
           repo={
             <a
               target="blank"
@@ -54,7 +60,11 @@ const ProjectSection = () => {
         <Project
           projectTitle="Sport DB"
           projectDescription=" Ein Gruppenprojekt, das eine öffentliche Sport-API nutzt, um verschiedene Sportarten zu explorieren. Mein Beitrag umfasste das Datenfetching von der API sowie die Implementierung eines benutzerfreundlichen Dropdown-Menüs für eine intuitive Navigation. Zusätzlich habe ich die API-Daten so aufbereitet, dass sie leicht verständlich und optisch ansprechend präsentiert werden, was die Nutzererfahrung erheblich verbessert hat."
-          technologies={[<React />, <JavaScript />, <Sass />]}
+          technologies={[
+            <React key="react" />,
+            <JavaScript key="javascript" />,
+            <Sass key="sass" />,
+          ]}
           repo={
             <a target="blank" href="https://github.com/KWiefel/SportsDB">
               <GitHub />
